d776d1161060dc569442acc8121911bba020baf6
Cliey <cyril.r38@gmail.com>
2019-10-30 22:20:55 +0100

    Renaming DataBaseManager as DatabaseManager

--- Managers/src/GeneralManager.cpp
+++ Managers/src/GeneralManager.cpp
@@ -19,7 +19,6 @@ namespace BookManager
         {
             registerHandlerFactory();
             settingsManager = SettingsManager::getSettingsManager();
-            SettingsManager::getSettingsManager();
             loadDatabase();
         }
 
