d1b2b02c23e182c8f775a46817a5522abfcb7700
Cliey <cyril.r38@gmail.com>
2020-05-24 17:49:36 +0200

    Create Category Tab with ListView (Fake data)
    Create Category tab and ListView
    Fake data in ListView
    
    Next Step :
    - Call to DatabaseManager to get Category from Database
    - Add a Category
    - Delete Category in Database
    - Add options in ContextMenu

--- GraphicalInterface/View/include/View/CategoryPage.hpp
+++ GraphicalInterface/View/include/View/CategoryPage.hpp
@@ -1,14 +1,27 @@
 #pragma once
+#include "Models/CategoryModel.hpp"
 #include <QWidget>
 #include <QGridLayout>
 #include <QLabel>
+#include <QListView>
+#include <QPoint>
+#include <QMenu>
 
 class CategoryPage : public QWidget
 {
+    Q_OBJECT
 public:
     CategoryPage(QWidget *parent = nullptr);
 
+private slots:
+    void deleteCategoriesSelected();
+    void openMenu(const QPoint& pos);
 private:
+    void initLeftPane();
+    void initList();
+
     QLabel* right;
-    QWidget* left;
+    QVBoxLayout* leftLayout;
+    CategoryModel* categoryModel;
+    QListView* categoryListView;
 };
--- GraphicalInterface/View/src/CategoryPage.cpp
+++ GraphicalInterface/View/src/CategoryPage.cpp
@@ -1,14 +1,96 @@
 #include "View/CategoryPage.hpp"
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QList>
+#include <QPushButton>
+#include <QMenu>
+#include <QAction>
 
+#include <QListWidgetItem>
+#include <iostream>
 
 CategoryPage::CategoryPage(QWidget *parent) : QWidget(parent)
 {
     QHBoxLayout* layout = new QHBoxLayout();
     right =  new QLabel("Something here later");
-    left =  new QWidget();
-    layout->addWidget(left, 50);
+    initLeftPane();
+    layout->addLayout(leftLayout, 50);
     layout->addWidget(right, 50);
 
     setLayout(layout);
+}
+
+void CategoryPage::initLeftPane()
+{
+    leftLayout = new QVBoxLayout();
+    initList();
+    QHBoxLayout* headerLayout = new QHBoxLayout();
+
+    QLabel* headerName = new QLabel("Categories");
+    QFont fontLabel;
+    fontLabel.setPointSize(16);
+    headerName->setFont(fontLabel);
+
+    QPushButton* buttonDeleteCategory = new QPushButton();
+    buttonDeleteCategory->setIcon(QIcon(":/icons/trash.png"));
+    buttonDeleteCategory->setFixedSize(28, 28);
+    buttonDeleteCategory->setIconSize(QSize(20, 20));
+    QObject::connect(buttonDeleteCategory, SIGNAL(clicked()), this, SLOT(deleteCategoriesSelected()));
+
+    headerLayout->addWidget(headerName);
+    headerLayout->addWidget(buttonDeleteCategory);
+
+    leftLayout->addLayout(headerLayout);
+    leftLayout->addWidget(categoryListView);
+}
+
+void CategoryPage::initList()
+{
+    QList<BookManager::Category::Category> categoryTest = {
+        {1, "Thriller"},
+        {2, "Sci-Fi"},
+        {3, "Fantasy"},
+        {4, "Young Adult"}};
+
+    // Here DatabaseManager -> getCategory
+
+    categoryModel = new CategoryModel(categoryTest);
+    categoryListView = new QListView();
+    categoryListView->setModel(categoryModel);
+    categoryListView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    categoryListView->setSelectionMode(QAbstractItemView::ExtendedSelection);
+    categoryListView->setContextMenuPolicy(Qt::CustomContextMenu);
+    QObject::connect(categoryListView, SIGNAL(customContextMenuRequested(const QPoint&)), this, SLOT(openMenu(const QPoint&)));
+}
+
+void CategoryPage::deleteCategoriesSelected()
+{
+    QItemSelectionModel* selection = categoryListView->selectionModel();
+    QModelIndexList listIndexes = selection->selectedIndexes();
+    std::sort(listIndexes.rbegin(), listIndexes.rend()); // reverse iterator, greater to lower
+    for(auto& index : listIndexes)
+    {
+        categoryModel->removeRow(index.row());
+    }
+}
+
+void CategoryPage::openMenu(const QPoint& pos)
+{
+    // Idea of the contextMenu
+    QMenu menuItem;
+    QAction* actionAdd = menuItem.addAction(tr("Add category", "Add a category"));
+    QAction* actionSearchBook = menuItem.addAction(tr("Search books", "Search book with the selected category"));
+    QAction* actionCopyCategoryName = menuItem.addAction(tr("Copy Name", "Copy category name"));
+
+    QModelIndex pointedItem = categoryListView->indexAt(pos);
+    QAction* selectedAction;
+    if(pointedItem.isValid())
+    {
+        selectedAction = menuItem.exec(QCursor::pos());
+        return;
+    }
+    actionSearchBook->setEnabled(false);
+    actionCopyCategoryName->setEnabled(false);
+    selectedAction = menuItem.exec(QCursor::pos());
+    return;
 }
