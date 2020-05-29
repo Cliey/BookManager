ae5ed9482d4e0a6ce283dd9ead52569524f2b361
Cliey <cyril.r38@gmail.com>
2020-05-29 15:09:39 +0200

    Add Header and Delete Action on Author Page
    Add Header with name of the page + delete button and Radio Button to filter.
    Add Delete Action with Popup if the author has book associated
    
    Next Step :
    - Add Filter function Author/Illustrator/All

--- GraphicalInterface/View/include/View/PersonPage.hpp
+++ GraphicalInterface/View/include/View/PersonPage.hpp
@@ -4,18 +4,27 @@
 #include <QWidget>
 #include <memory>
 
-class QTableWidget;
 class QLabel;
 class QVBoxLayout;
+class QHBoxLayout;
+class QTableView;
 
 class PersonPage : public QWidget
 {
+    Q_OBJECT
 public:
     PersonPage(std::shared_ptr<BookManager::Manager::DatabaseManager> databaseManager, QWidget *parent = nullptr);
 
+private slots:
+    void deletePersonsSelected();
 
 private:
     void initLeftPane();
+    void initPersonList();
+    QHBoxLayout* initLeftPaneHeader();
+    QHBoxLayout* initFilterButton();
+    void deletePersonSelected(const QModelIndex& index);
+
 
 
     std::shared_ptr<BookManager::Manager::DatabaseManager> databaseManager;
@@ -23,4 +32,5 @@ private:
     QLabel* right;
     QVBoxLayout* leftLayout;
     PersonModel* personModel;
+    QTableView* personTableView;
 };
--- GraphicalInterface/View/src/PersonPage.cpp
+++ GraphicalInterface/View/src/PersonPage.cpp
@@ -1,10 +1,14 @@
 #include "View/PersonPage.hpp"
+#include "Utils/Exceptions.hpp"
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QTableView>
 #include <QTableWidget>
 #include <QHeaderView>
 #include <QLabel>
-#include <QVBoxLayout>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QMessageBox>
 #include <iostream>
 
 PersonPage::PersonPage(std::shared_ptr<BookManager::Manager::DatabaseManager> databaseManager, QWidget *parent)
@@ -23,16 +27,92 @@ void PersonPage::initLeftPane()
 {
     leftLayout = new QVBoxLayout();
 
+    initPersonList();
+
+    leftLayout->addLayout(initLeftPaneHeader());
+    leftLayout->addLayout(initFilterButton());
+    leftLayout->addWidget(personTableView);
+}
+
+void PersonPage::initPersonList()
+{
     auto personVector = databaseManager->getPersonVector(20, 0);
     QList<BookManager::Entity::Person> personList(personVector.begin(), personVector.end());
 
     personModel = new PersonModel(personList);
-    QTableView* personTableView = new QTableView();
+    personTableView = new QTableView();
 
     personTableView->setModel(personModel);
     personTableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
     personTableView->setSelectionBehavior(QAbstractItemView::SelectRows);
     personTableView->verticalHeader()->hide();
-
-    leftLayout->addWidget(personTableView);
+    personTableView->setSortingEnabled(true);
 }
+
+QHBoxLayout* PersonPage::initLeftPaneHeader()
+{
+    QHBoxLayout* headerLayout = new QHBoxLayout();
+    QLabel* headerName = new QLabel("Author/Illustrator");
+    QFont fontLabel;
+    fontLabel.setPointSize(16);
+    headerName->setFont(fontLabel);
+
+    QPushButton* buttonDeletePerson = new QPushButton();
+    buttonDeletePerson->setIcon(QIcon(":/icons/trash.png"));
+    buttonDeletePerson->setFixedSize(28, 28);
+    buttonDeletePerson->setIconSize(QSize(20, 20));
+    QObject::connect(buttonDeletePerson, &QPushButton::clicked, this, &PersonPage::deletePersonsSelected);
+
+    headerLayout->addWidget(headerName);
+    headerLayout->addWidget(buttonDeletePerson);
+    return headerLayout;
+}
+
+QHBoxLayout* PersonPage::initFilterButton()
+{
+    QHBoxLayout* filterButtonLayout = new QHBoxLayout();
+    QRadioButton* buttonAllFilter = new QRadioButton("All");
+    QRadioButton* buttonAuthorFilter = new QRadioButton("Author");
+    QRadioButton* buttonIllustratorFilter = new QRadioButton("Illustrator");
+    filterButtonLayout->addWidget(buttonAllFilter);
+    filterButtonLayout->addWidget(buttonAuthorFilter);
+    filterButtonLayout->addWidget(buttonIllustratorFilter);
+    return filterButtonLayout;
+}
+
+void PersonPage::deletePersonsSelected()
+{
+    QItemSelectionModel* selection = personTableView->selectionModel();
+    QModelIndexList listIndexes = selection->selectedRows();
+    std::sort(listIndexes.rbegin(), listIndexes.rend()); // reverse iterator, greater to lower
+    for(auto& index : listIndexes)
+    {
+        deletePersonSelected(index);
+    }
+
+}
+
+void PersonPage::deletePersonSelected(const QModelIndex& index)
+{
+    auto personSelected = personModel->data(index, PersonModel::PersonModel::PersonObject).value<BookManager::Entity::Person>();
+    try
+    {
+        databaseManager->deletePerson(personSelected.getId(), false);
+        personModel->removeRow(index.row());
+        return;
+    }
+    catch(const Utils::Exceptions::EC_ForeignKeyFound& e)
+    {
+        auto response = QMessageBox::critical(this, "Error deleting an Author" , e.getStr(), QMessageBox::Yes | QMessageBox::No);
+        if(response == QMessageBox::Yes)
+        {
+            e.executeCallback();
+            personModel->removeRow(index.row());
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
